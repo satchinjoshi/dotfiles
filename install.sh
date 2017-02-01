b8886f620c7d7107b2015e7873a7871b335be826
Sachin Joshi <satchin.joshi@gmail.com>
2017-02-02 02:42:17 +0545

    Automatic backup @ Thu Feb  2 02:42:17 NPT 2017

--- install.sh
+++ install.sh
@@ -8,3 +8,4 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 mkdir -p ~/.config/nvim
 mkdir -p ~/.config/nvim/backup
 ln -s ~/dotfiles/.vimrc ~/.config/nvim/init.vim
+ln -s ~/dotfiles/.zshrc ~/.zshrc
