a7ef5d0275655d0592d191f976bea8a6961218da
Sachin Joshi <satchin.joshi@gmail.com>
2017-02-18 14:57:40 +0545

    Automatic backup @ Sat Feb 18 14:57:40 NPT 2017

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
-sudo add-apt-repository ppa:neovim-ppa/unstable -y
-sudo apt-get update -y
-sudo apt-get install neovim -y
+if output=$(which pacman); then
+    yes | sudo pacman -S neovim
+elif output=$(which apt-get); then
+    sudo add-apt-repository ppa:neovim-ppa/unstable -y
+    sudo apt-get update -y
+    sudo apt-get install neovim -y
+else
+    printf 'update the code for os\n'
+    exit 0
+fi
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -10,3 +17,4 @@ mkdir -p ~/.config/nvim/backup
 ln -s ~/dotfiles/.vimrc ~/.config/nvim/init.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
